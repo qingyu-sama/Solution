6e889f88ab4e6020355d9c615b0935dc8c4a1e4f
qingyu <1470078125@qq.com>
2021-12-13 10:52:21 +0800

    多数元素

--- Solution.c
+++ Solution.c
@@ -86,6 +86,22 @@ void sortColors(int* nums, int numsSize) {
 	}
 }
 
+// 169. 多数元素
+int majorityElement(int* nums, int numsSize) {
+	int m = nums[0];
+	int vc = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		if (nums[i] == m) vc++;
+		else vc--;
+		if (vc == 0) {
+			m = nums[i];
+			vc = 1;
+		}
+	}
+	return m;
+}
+
 #pragma endregion
 
 #pragma region 二叉树题
