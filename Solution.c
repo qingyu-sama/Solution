fcda633423718fc4f56d35c2ac788982ca581bf7
qingyu <1470078125@qq.com>
2021-12-13 10:09:02 +0800

    环形链表

--- Solution.c
+++ Solution.c
@@ -202,7 +202,7 @@ struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* he
 	return p1;
 }
 
-// 141. 环形链表
+// 142. 环形链表 II
 struct ListNode* detectCycle(struct ListNode* head) {
 	struct ListNode* p1 = head, * p2 = head;
 	while (p1 != NULL && p1->next != NULL)
@@ -221,5 +221,19 @@ CON:
 	}
 	return p1;
 }
+
+// 142. 环形链表
+bool hasCycle(struct ListNode* head) {
+	struct ListNode* p1 = head, * p2 = head;
+	while (p1 != NULL && p1->next != NULL)
+	{
+		p1 = p1->next->next;
+		p2 = p2->next;
+		if (p1 == p2) return true;
+	}
+	return false;
+}
+
+// 142. 环形链表 II
 
 #pragma endregion
