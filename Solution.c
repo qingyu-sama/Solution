11679daabe1df3f8ef83713486ca5581a4cc9108
qingyu <1470078125@qq.com>
2021-12-13 12:22:24 +0800

    爬楼梯

--- Solution.c
+++ Solution.c
@@ -218,6 +218,17 @@ int maxSubArray(int* nums, int numsSize) {
 	return maxAns;
 }
 
+// 70. ÅÀÂ¥Ìİ
+int climbStairs(int n) {
+	if (n <= 3) return n;
+	int p1 = 2, p2 = 3;
+	for (int i = 3; i < n; i++) {
+		p2 += p1;
+		p1 = p2 - p1;
+	}
+	return p2;
+}
+
 #pragma endregion
 
 #pragma region ¶ş²æÊ÷Ìâ
